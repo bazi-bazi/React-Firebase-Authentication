08d3817e0ce9fc2b46197542d4a430b8d7cef4ac
bazi <vazha.davituliani@gmail.com>
2020-10-20 13:44:11 +0400

    basic structure of firebase and ui

---
+++ src/components/Signup.js
@@ -0,0 +1,41 @@
+import React, {useRef} from "react";
+import { Form, Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const Signup =()=> {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+
+  return (
+      <React.Fragment>
+          <Card.Body>
+              <h1  className="text-center mb-4">Sign Up</h1>
+              <Form>
+                  <Form.Group id="email">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control type="email" ref={emailRef} required />
+                  </Form.Group>
+                  <Form.Group id="password">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" ref={passwordRef} required />
+                  </Form.Group>
+                  <Form.Group id="password-confirmation">
+                      <Form.Label>Password Confirmation</Form.Label>
+                      <Form.Control type="password" ref={passwordConfirmRef} required />
+                  </Form.Group>
+                  <Button className="w-100" type="submit">Sign Up</Button>
+              </Form>
+             
+          </Card.Body>
+          <div className="w-100 text-center mt-2">
+              Already have an account? Log in
+          </div>
+
+      </React.Fragment>
+  )
+}
+
+export default Signup;
+  
